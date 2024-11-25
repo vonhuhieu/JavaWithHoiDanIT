fbcdb747b47204fb42cf8c9ef8835cd3c2ec2d2a
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-25 20:08:42 +0700

    Tìm hiểu về Integer

---
+++ src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 9;
+        int c = a + b;
+        int hoidanit = 26;
+        int test = 5 + 9*3 - 10/2;
+        System.out.println("Sum: " + c);
+        System.out.println("test: " + test);
+    }
+}
