cb55d3418b702767fab3beba834afad635964a50
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-25 20:43:43 +0700

    Làm quen với Sacnner

---
+++ src/InputFunction.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class InputFunction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Your name is: ");
+        String name = scanner.nextLine();
+        System.out.println("Your age is: ");
+        int age = scanner.nextInt();
+        System.out.println("My name is " + name + ", age = " + age);
+        scanner.close();
+    }
+}
