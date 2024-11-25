0edb9b9600bca82df58196696bc7d5f3187e9384
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-25 20:59:58 +0700

    Làm quen với một vài Math functions

---
+++ src/MathFunctions.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MathFunctions {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // pow trả dữ liệu dưới dạng số thực
+        double a = Math.pow(2, 6);
+        System.out.println("Mời nhập số a: " + a);
+        // int a = scanner.nextInt();
+        double b = Math.sqrt(10000);
+        System.out.println("Mời nhập số b: " + b);
+        // int b = scanner.nextInt();
+        double c = Math.min(a, b);
+        System.out.println("Min = " + c);
+    }
+}
