5eab41c533fd61723c7222ed35a961d780e53c5b
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-28 02:53:22 +0700

    Làm thêm bài tập về exception

--- src/KeywordFinally/Lab/Test.java
+++ src/KeywordFinally/Lab/Test.java
@@ -7,34 +7,30 @@ public class Test {
         Scanner scanner = new Scanner(System.in);
         while (true){
             try {
-                System.out.println("Mời nhập MSSV: ");
+                System.out.println("Mời nhập mã SV: ");
                 String maSV = scanner.nextLine();
-                System.out.println("Mời nhập tên: ");
+                System.out.println("Mời nhập tên SV: ");
                 String name = scanner.nextLine();
-                System.out.println("Mời nhập điểm: ");
+                System.out.println("Mời nhập điểm sinh viên: ");
                 double diem = scanner.nextDouble();
-                System.out.println("Mời nhập tuổi: ");
+                scanner.nextLine();
+                System.out.println("Mời nhập tuổi SV: ");
                 int age = scanner.nextInt();
+                scanner.nextLine();
                 if((diem > 0 && diem < 10) && (age > 18 && age < 100)){
-                    SinhVien newSV = new SinhVien(maSV, name, diem, age);
-                    System.out.println(newSV);
+                    SinhVien newSinhVien = new SinhVien(maSV, name, diem, age);
+                    System.out.println(newSinhVien);
                     break;
                 }
                 else {
-                    System.out.println("Dữ liệu nhập không hợp lệ. Xin kiểm tra lại");
-                    System.out.println("Đểm phải từ 0 đến 10");
-                    System.out.println("Tuổi phải lớn hơn 18 và nhỏ hơn 100");
+                    System.out.println("Kiểm tra du lieu nhập vào có hợp lệ");
+                    System.out.println("Điểm phải lớn hơn 0 và nhỏ hơn 10");
+                    System.out.println("Tuổi phải lon hon 18 và nhỏ hơn 100");
                 }
             } catch (Exception e) {
-                scanner.next();
-                System.out.println("Kiểm tra dữ liệu nhập vào có hợp lệ hay không?");
-                System.out.println("MSSV phải nhập một chuỗi");
-                System.out.println("Tên phải nhập một chuỗi");
-                System.out.println("Điểm phải là số thực");
-                System.out.println("Tuổi phải là số nguyên");
-            }
-            finally {
-                scanner.close();
+                System.out.println("Kiểm tra xem dữ liệu nhập vào có hợp lệ");
+                System.out.println("Điểm phải là số thực. Tuổi phải là số nguyên");
+                scanner.nextLine();
             }
         }
     }
